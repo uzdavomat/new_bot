1cbc12ba8d706191b7f26bf3645eb1a3dcebcd2e
ami834-oss <amirhanadilbaev27@gmail.com>
2026-03-14 09:51:32 +0500

    new bot

---
+++ main.py
@@ -0,0 +1,23 @@
+import asyncio
+from aiogram import Bot, Dispatcher
+from aiogram.types import Message
+from aiogram.filters import  Command
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+TOKEN=os.getenv('BOT_TOKEN')
+bot = Bot(token=TOKEN)
+dp = Dispatcher()
+
+@dp.message(Command('start'))
+async def start(message: Message):
+    await message.answer(f"hello {message.from_user.first_name}")
+
+
+async def main():
+    await dp.start_polling(bot)
+
+if __name__ == '__main__':
+    asyncio.run(main())
+
